8e3db1feebaf7c8444eadf90038a7b8c3ca64605
kulyomza <kulyomzaigor@gmail.com>
2023-11-14 21:42:38 +0200

    lesson21

--- src/test/java/initForWebDriver/WebDriverInit.java
+++ src/test/java/initForWebDriver/WebDriverInit.java
@@ -3,6 +3,7 @@ package initForWebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -16,8 +17,11 @@ public class WebDriverInit {
     @BeforeTest
     public void initDriver() {
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--disable-notifications");
+        driver = new ChromeDriver(chromeOptions);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
     @AfterTest
     public void after(){
