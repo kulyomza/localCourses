ce4fc9e028a431cddcec6aebbca04db5d946eabf
kulyomza <kulyomzaigor@gmail.com>
2023-12-12 19:52:00 +0200

    homeWork26_updates

--- src/test/java/homeWorkTests/hm25/SelenidePageObjectHomeWork.java
+++ src/test/java/homeWorkTests/hm25/SelenidePageObjectHomeWork.java
@@ -11,12 +11,13 @@ import static com.codeborne.selenide.Selenide.open;
 public class SelenidePageObjectHomeWork extends InitialSelenide {
 
     final String URL = "https://rozetka.com.ua/ua/";
+    final String COOKIE_VALUE = "qMVG4EbmTavyakgf469R7wP.QJswWp1.pHAUShex1J8-1702129305-0-1-7a105317.4ee41f07.2d110cf7-0.2.1702129305";
     final String EXPECTED_CART_COUNTER = "1";
 
     @Test
     public void testAddProductToCartAndQuantity(){
         open(URL);
-        rozetkaCFCookie("qMVG4EbmTavyakgf469R7wP.QJswWp1.pHAUShex1J8-1702129305-0-1-7a105317.4ee41f07.2d110cf7-0.2.1702129305");
+        rozetkaCFCookie(COOKIE_VALUE);
         String firstTitleOfProductOnListing = new MainPageLogic().clickOnCategory(0)
                 .clickOnSubCategoryLaptops().productTitleFromList(0);
         String addedProductToBasket = new LaptopCategoryPageLogic().addProductToBasket(0).checkingCartCounter(EXPECTED_CART_COUNTER).openCartPopup()
